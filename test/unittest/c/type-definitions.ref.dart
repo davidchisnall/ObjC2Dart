6763867359926e4139a5187f792975bb6dd83c9e
Vlad Tataranu <vlad.tataranu.dev@gmail.com>
2014-04-23 02:17:41 +0300

    Moved types test to type-definitions to make way for functionality test

---
+++ test/unittest/c/type-definitions.ref.dart
@@ -0,0 +1,15 @@
+import 'package:this/c/types.dart';
+void f() {
+}
+void main() {
+  C__TYPE_Int64 main() {
+    C__TYPE_Int64 i;
+    C__TYPE_Int64 i1;
+    C__TYPE_Int64 i2;
+    var c;
+    class_1 c1;
+    class_1 c2;
+    class_1 c3;
+  }
+  main();
+}
