d909aca4e114bc7a500395b005793d5baf9be157
Vlad Tataranu <vlad.tataranu.dev@gmail.com>
2014-04-23 00:38:52 +0300

    Added some more tests for types

--- dart/test/c/types-test.dart
+++ dart/test/c/types-test.dart
@@ -4,11 +4,17 @@ import 'package:unittest/unittest.dart';
 
 void main() {
   group('int64', () {
-    test('Allocate and assign a literal', () {
+    test('Allocate and assign literals', () {
       C__TYPE_Int64 a = C__TYPE_DEFINITION.int64_t.create(
           new C__Memory(C__TYPE_DEFINITION.int64_t.byteSize), 0);
       a.setLiteral(10);
       expect(a.getLiteral(), equals(10));
+      a.setLiteral(-10);
+      expect(a.getLiteral(), equals(-10));
+      a.setLiteral(343597383680);
+      expect(a.getLiteral(), equals(343597383680));
+      a.setLiteral(-343597383680);
+      expect(a.getLiteral(), equals(-343597383680));
     });
   });
 }
