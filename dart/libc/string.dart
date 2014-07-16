6c4a22d8e82a4bb6f358520a887f4b0da0dab66a
David Chisnall <theraven@theravensnest.org>
2014-07-16 12:33:37 +0100

    Update memcpy and memset for new runtime APIs.

--- dart/libc/string.dart
+++ dart/libc/string.dart
@@ -4,10 +4,11 @@ import '../c/types.dart';
  * Copies |num| bytes of memory from |source| to |destination|.
  * Returns |destination|.
  */
-C__TYPE_Pointer C__memcpy(C__TYPE_Pointer destination, C__TYPE_Pointer source, int num) {
-  for (int i = 0; i < num; ++i) {
-    destination.pointee.view.setInt8(i, source.pointee.view.getInt8(i));
-  }
+DartCPointer C__memcpy(DartCPointer destination, DartCPointer source, DartCInteger num) {
+  DartCObject dest = destination.dereference();
+  DartCObject src = source.dereference();
+  int bytes = num.intValue();
+  dest.memory.memcpy(src.memory, dest.offset, src.offset, bytes);
   return destination;
 }
 
@@ -15,9 +16,12 @@ C__TYPE_Pointer C__memcpy(C__TYPE_Pointer destination, C__TYPE_Pointer source, i
  * Sets |num| bytes of memory starting at |ptr| to |value|.
  * Returns |ptr|.
  */
-C__TYPE_Pointer C__memset(C__TYPE_Pointer ptr, int value, int num) {
-  for (int i = 0; i < num; ++i) {
-    ptr.pointee.view.setInt8(i, value);
+DartCPointer C__memset(DartCPointer ptr, DartCInteger value, DartCInteger len) {
+  DartCObject dest = ptr.dereference();
+  int bytes = len.intValue();
+  int byte = value.intValue().toSigned(8);
+  for (int i = 0; i < bytes; ++i) {
+    dest.memory.setInt8(i, byte);
   }
   return ptr;
 }
