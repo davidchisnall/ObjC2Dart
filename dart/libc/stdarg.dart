6bd55f31aba2f0da5ab15f15e64f53bba5978ebe
David Chisnall <theraven@theravensnest.org>
2014-07-18 11:53:34 +0100

    Implement variadic argument support functions.

--- dart/libc/stdarg.dart
+++ dart/libc/stdarg.dart
@@ -1,37 +1,26 @@
-/**
- * Abstract superclass to be used in simulating variadic functions.
- * 
- * To simulate a variadic function subclass and override [body].
- */ 
-abstract class C__VARARGS_FUNCTION {
-  // Override in subclass to provide the function body.
-  dynamic body(List arguments);
-  
-  // Redirect message, exposing (positional) arguments.
-  dynamic noSuchMethod(Invocation invocation) {
-    return body(invocation.positionalArguments);
-  }
-}
 
 class va_list {
-  List _arguments;
-  int _position;
-  
-  void initialize(List arguments, int position) {
-    _arguments = arguments;
-    _position = position;
+  List arguments;
+  int position;
+
+  void initialize(List args, int pos) {
+    arguments = args;
+    position = pos;
   }
-  
+
   Object next() {
-    ++_position;
-    return _arguments[_position - 1].pointer();
+    return arguments[position++].pointer();
   }
 }
 
-void va_start(va_list ap, int argn, List args) {
-  ap.initialize(args, argn);
+void __builtin_va_start(va_list ap, List args) {
+  ap.initialize(args, 0);
 }
 
-dynamic va_arg(va_list ap) {
+dynamic __builtin_va_arg(va_list ap) {
   return ap.next();
+}
+
+void __builtin_va_copy(va_list dest, va_list src) {
+  dest.initialize(src.arguments, src.position);
 }
