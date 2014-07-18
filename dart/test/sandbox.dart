01c11f4794b6fef62c19579999800d0897089112
David Chisnall <theraven@theravensnest.org>
2014-07-18 12:33:25 +0100

    Update sandbox.dart for new APIs.

--- dart/test/sandbox.dart
+++ dart/test/sandbox.dart
@@ -1,37 +1,34 @@
-import 'package:this/c/types.dart';
-import 'package:this/libc/stdlib.dart';
-import 'package:this/libc/stdarg.dart';
+import '../libc/libc.dart';
 
-import 'package:this/c/types.dart';
 import 'dart:io';
 
-C__TYPE_Int64 binsearch(C__TYPE_Pointer v, C__TYPE_Int64 start, C__TYPE_Int64 end, C__TYPE_Int64 s) {
+DartCSignedLong binsearch(DartCPointer v, DartCSignedLong start, DartCSignedLong end, DartCSignedLong s) {
   if (start == end) {
     return start;
   }
    else ;
-  C__TYPE_Int64 middle = start + end / (new C__TYPE_Int64.literal(2));
+  DartCSignedLong middle = start + end / (new DartCSignedLong.fromInt(2));
   if (v.index(middle) < s) {
-    return binsearch(v, middle + (new C__TYPE_Int64.literal(1)), end, s);
+    return binsearch(v, middle + (new DartCSignedLong.fromInt(1)), end, s);
   }
    else {
     return binsearch(v, start, middle, s);
   }
   ;
 }
-void test(C__TYPE_Int64 n) {
-  C__TYPE_Pointer v = malloc(n * (new C__TYPE_Int64.literal(8)));
-  for (C__TYPE_Int64 i = (new C__TYPE_Int64.literal(0)); i < n; i.inc()) {
+void test(DartCSignedLong n) {
+  DartCPointer v = malloc(n * (new DartCSignedLong.fromInt(8)));
+  for (DartCSignedLong i = (new DartCSignedLong.fromInt(0)); i < n; i.inc()) {
     v.index(i).set(i);
   }
   ;
-  C__TYPE_Int64 p = binsearch(v, (new C__TYPE_Int64.literal(0)), n, n / (new C__TYPE_Int64.literal(3)));
+  DartCSignedLong p = binsearch(v, (new DartCSignedLong.fromInt(0)), n, n / (new DartCSignedLong.fromInt(3)));
   free(v);
 }
 
 
 void main() {
   DateTime t = new DateTime.now();
-  test(new C__TYPE_Int64.literal(200));
+  test(new DartCSignedLong.fromInt(200));
   stdout.write("${(new DateTime.now()).difference(t).inMilliseconds} ");
 }
